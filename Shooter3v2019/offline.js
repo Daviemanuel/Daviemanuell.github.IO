3e5626e60eb166da8b3fcb4a0945eec5d746e18d
Daviemanuel <50117391+Daviemanuel@users.noreply.github.com>
2019-06-02 21:38:34 -0300

    Add files via upload

---
+++ Shooter3v2019/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1559521906,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowupbutton-sheet0.png",
+		"images/arrowrightbutton2-sheet0.png",
+		"images/arrowleftbutton2-sheet0.png",
+		"images/arrowdownbutton2-sheet0.png",
+		"images/background-sheet0.png",
+		"images/background-sheet1.png",
+		"images/enemy-sheet0.png",
+		"images/bulletcontrollerenemy-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/powerup-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
